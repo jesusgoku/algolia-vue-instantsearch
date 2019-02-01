7b7d5b7682a4a10734a7b2d602f896528971e869
Jesus Urrutia <jesus.urrutia@gmail.com>
2019-02-01 18:20:52 -0300

    feat: first basic example of render elements

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+    <ais-index
+      :app-id="appId"
+      :api-key="appKey"
+      :index-name="indexName"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <ais-search-box></ais-search-box>
+
+            <custom></custom>
+
+            <h4>Manufacturer</h4>
+            <ais-refinement-list attribute-name="manufacturer"></ais-refinement-list>
+
+            <h4>Type</h4>
+            <ais-refinement-list attribute-name="type"></ais-refinement-list>
+          </div>
+
+          <div class="col-9">
+            <ais-results>
+              <template slot-scope="{ result }">
+                <h2>
+                  <ais-highlight :result="result" attribute-name="name"></ais-highlight>
+                </h2>
+              </template>
+            </ais-results>
+          </div>
+        </div>
+      </div>
+
+    </ais-index>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Custom from './components/Custom';
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
-    HelloWorld,
+    Custom,
+  },
+
+  data() {
+    return {
+      appId: process.env.VUE_APP_ALGOLIA_APP_ID,
+      appKey: process.env.VUE_APP_ALGOLIA_APP_KEY,
+      indexName: process.env.VUE_APP_ALGOLIA_INDEX_NAME,
+    };
   },
 };
 </script>
 
 <style>
-#app {
+.App {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
